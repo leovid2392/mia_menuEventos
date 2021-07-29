aa78ea98495af8368af7b9caccb716159fd3f6b3
Leotori <leovid2392@gmail.com>
2021-07-29 16:00:14 -0500

    slider style and functionallity added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import logo_miaBySelina from "./assets/logo_mia_by_selina.png";
 
 import data from "./data";
+
+import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronRight } from "react-icons/fi";
 
 import "./App.css";
 
 function App() {
 	const [events, setEvents] = useState(data);
 	const [index, setIndex] = useState(0);
+
+	useEffect(() => {
+		const lastIndex = events.length - 1;
+		if (index < 0) {
+			setIndex(lastIndex);
+		}
+		if (index > lastIndex) {
+			setIndex(0);
+		}
+	}, [index, events]);
+
+	useEffect(() => {
+		let slider = setTimeout(() => {
+			setIndex(index + 1);
+		}, 4000);
+		return () => {
+			clearInterval(slider);
+		};
+	}, [index]);
+
+	const handlePrevBtn = () => {
+		setIndex(index - 1);
+	};
+
+	const handleNextBtn = () => {
+		setIndex(index + 1);
+	};
 
 	return (
 		<div className='App'>
@@ -25,9 +55,20 @@ function App() {
 			<section className='slider'>
 				{events.map((event, eventIndex) => {
 					const { id, url, title, image } = event;
+					let position = "nextSlide";
+
+					if (eventIndex === index) {
+						position = "activeSlide";
+					}
+					if (
+						eventIndex === index - 1 ||
+						(index === 0 && eventIndex === events.length - 1)
+					) {
+						position = "lastSlide";
+					}
 
 					return (
-						<article key={id}>
+						<article className={position} key={id}>
 							<figure className='slider_imgContainer'>
 								<a className='slider_link' href={url}>
 									<img className='slider_img' src={image} alt={title} />
@@ -39,15 +80,12 @@ function App() {
 						</article>
 					);
 				})}
-
-				{/* <figure className='slider_imgContainer'>
-					<a className='slider_link' href='#'>
-						<img className='slider_img' src={july_31} alt='' />
-						<div className='slider_buyText'>
-							<h5>click me to buy</h5>
-						</div>
-					</a>
-				</figure> */}
+				<button className='slider_prevBtn' onClick={handlePrevBtn}>
+					<FiChevronLeft />
+				</button>
+				<button className='slider_nextBtn' onClick={handleNextBtn}>
+					<FiChevronRight />
+				</button>
 			</section>
 		</div>
 	);
