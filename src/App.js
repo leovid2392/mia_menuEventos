449f026a02bdd29139b06250f897c8300cfe1f85
Leotori <leovid2392@gmail.com>
2021-07-29 14:55:40 -0500

    App setup and slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
+import React, { useState } from "react";
+
+import logo_miaBySelina from "./assets/logo_mia_by_selina.png";
+
+import data from "./data";
+
 import "./App.css";
 
 function App() {
+	const [events, setEvents] = useState(data);
+	const [index, setIndex] = useState(0);
+
 	return (
 		<div className='App'>
-			<h1>menu events mia</h1>
+			<header className='home_header'>
+				<figure className='home_logoContainer'>
+					<img
+						className='home_logo'
+						src={logo_miaBySelina}
+						alt='mia by selina'
+					/>
+				</figure>
+			</header>
+			<button>menu</button>
+			<section className='slider'>
+				{events.map((event, eventIndex) => {
+					const { id, url, title, image } = event;
+
+					return (
+						<article key={id}>
+							<figure className='slider_imgContainer'>
+								<a className='slider_link' href={url}>
+									<img className='slider_img' src={image} alt={title} />
+									<div className='slider_buyText'>
+										<h5>click me to buy</h5>
+									</div>
+								</a>
+							</figure>
+						</article>
+					);
+				})}
+
+				{/* <figure className='slider_imgContainer'>
+					<a className='slider_link' href='#'>
+						<img className='slider_img' src={july_31} alt='' />
+						<div className='slider_buyText'>
+							<h5>click me to buy</h5>
+						</div>
+					</a>
+				</figure> */}
+			</section>
 		</div>
 	);
 }
+
+// const sliderData = [
+// 	{
+// 		id: 1,
+// 		url: "https://www.ticketfairy.com/event/paulor-31jul2021/",
+// 		title: "paulor neo human, special dinner",
+// 		image: july_31,
+// 	},
+// 	{
+// 		id: 2,
+// 		url: "https://www.ticketfairy.com/event/whomadewho-4aug2021/",
+// 		title: "who made who by sam sparacio, special dinner",
+// 		image: august_04,
+// 	},
+// 	{
+// 		id: 3,
+// 		url: "https://www.ticketfairy.com/event/mathame-7aug2021/",
+// 		title: "mathame lyke neo human, special dinner",
+// 		image: august_07,
+// 	},
+// ];
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,25 @@
+const sliderData = [
+	{
+		id: 1,
+		url: "https://www.ticketfairy.com/event/paulor-31jul2021/",
+		title: "paulor neo human, special dinner",
+		image:
+			"https://res.cloudinary.com/leovid23/image/upload/v1627585486/mia/july_31_zdogrx.jpg",
+	},
+	{
+		id: 2,
+		url: "https://www.ticketfairy.com/event/whomadewho-4aug2021/",
+		title: "who made who by sam sparacio, special dinner",
+		image:
+			"https://res.cloudinary.com/leovid23/image/upload/v1627585487/mia/august_04_u39ffo.jpg",
+	},
+	{
+		id: 3,
+		url: "https://www.ticketfairy.com/event/mathame-7aug2021/",
+		title: "mathame lyke neo human, special dinner",
+		image:
+			"https://res.cloudinary.com/leovid23/image/upload/v1627585487/mia/august_07_tdkiim.jpg",
+	},
+];
+
+export default sliderData;
