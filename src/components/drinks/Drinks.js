df5ad619ed103fce333a2844fb965393339fe338
Leotori <leovid2392@gmail.com>
2021-07-31 11:42:35 -0500

    menu ready to feedback

--- src/components/drinks/Drinks.js
+++ src/components/drinks/Drinks.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-function Drinks({ img1, img2 }) {
+function Drinks({ img1, img2, legalsText }) {
 	return (
 		<section className='drinks'>
+			<p className='taxInfo'>{legalsText}</p>
 			<figure className='menuImg_container'>
 				<img src={img1} alt='menu drinks' />
 				<img src={img2} alt='menu drinks' />
--- src/components/food/Food.js
+++ src/components/food/Food.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-function Food({ img }) {
+function Food({ img, legalsText }) {
 	return (
 		<section className='food'>
+			<p className='taxInfo'>{legalsText}</p>
 			<figure className='menuImg_container'>
 				<img src={img} alt='food menu' />
 			</figure>
