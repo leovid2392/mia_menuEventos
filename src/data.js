a620d6272af5949b331b4f9a80fa3f6e6603acba
Leotori <leovid2392@gmail.com>
2021-08-09 09:21:16 -0500

    adding august_14 event image, waiting for links

--- src/data.js
+++ src/data.js
@@ -9,6 +9,13 @@ const sliderData = [
 	{
 		id: 2,
 		url: "#",
+		title: "cabizbajo, special dinner",
+		image:
+			"https://res.cloudinary.com/leovid23/image/upload/v1628516609/mia/august_14_awtfqx.jpg",
+	},
+	{
+		id: 3,
+		url: "#",
 		title: "Nic fanciulli, special dinner",
 		image:
 			"https://res.cloudinary.com/leovid23/image/upload/v1628476747/mia/august_15_aaishg.jpg",
