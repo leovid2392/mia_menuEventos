e0d9f9499ae26536be0a4ddcba0860184923b039
Leotori <leovid2392@gmail.com>
2021-08-10 22:45:47 -0500

    adding links to august11,14,15 events

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 const sliderData = [
 	{
 		id: 1,
-		url: "#",
+		url: "https://www.eventbrite.com.mx/e/mystical-sunset-tickets-166172232621",
 		title: "mistical sunset, agust 14th",
 		image:
 			"https://res.cloudinary.com/leovid23/image/upload/v1628476748/mia/august_11_jvah1i.jpg",
 	},
 	{
 		id: 2,
-		url: "#",
+		url: "https://www.ticketfairy.com/event/cabizbajo-14aug2021/",
 		title: "cabizbajo, special dinner",
 		image:
 			"https://res.cloudinary.com/leovid23/image/upload/v1628516609/mia/august_14_awtfqx.jpg",
 	},
 	{
 		id: 3,
-		url: "#",
+		url: "https://www.ticketfairy.com/event/nic-fanciulli-15aug2021/",
 		title: "Nic fanciulli, special dinner",
 		image:
 			"https://res.cloudinary.com/leovid23/image/upload/v1628476747/mia/august_15_aaishg.jpg",
