7d70094fc258e6686aeb2e95af5813573db09feb
Leotori <leovid2392@gmail.com>
2021-08-12 09:22:25 -0500

    removing august_11 event

--- src/data.js
+++ src/data.js
@@ -1,20 +1,13 @@
 const sliderData = [
 	{
 		id: 1,
-		url: "https://www.eventbrite.com.mx/e/mystical-sunset-tickets-166172232621",
-		title: "mistical sunset, agust 14th",
-		image:
-			"https://res.cloudinary.com/leovid23/image/upload/v1628476748/mia/august_11_jvah1i.jpg",
-	},
-	{
-		id: 2,
 		url: "https://www.ticketfairy.com/event/cabizbajo-14aug2021/",
 		title: "cabizbajo, special dinner",
 		image:
 			"https://res.cloudinary.com/leovid23/image/upload/v1628516609/mia/august_14_awtfqx.jpg",
 	},
 	{
-		id: 3,
+		id: 2,
 		url: "https://www.ticketfairy.com/event/nic-fanciulli-15aug2021/",
 		title: "Nic fanciulli, special dinner",
 		image:
