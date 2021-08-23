ef18cfeb2edd1a05859858a7ea1f89901f562032
Leotori <leovid2392@gmail.com>
2021-08-23 07:32:43 -0500

    august 25 event added

--- src/data.js
+++ src/data.js
@@ -1,17 +1,18 @@
+import React from "react";
+import august_25 from "./assets/slider/august_25.jpeg";
+
 const sliderData = [
 	{
 		id: 1,
 		url: "#",
-		title: "drink",
-		image:
-			"https://res.cloudinary.com/leovid23/image/upload/v1629134401/mia/default_ju2zv9.jpg",
+		title: "august 25 event, mystical sunset",
+		image: august_25,
 	},
 	{
 		id: 2,
 		url: "#",
-		title: "drink",
-		image:
-			"https://res.cloudinary.com/leovid23/image/upload/v1629134401/mia/default_ju2zv9.jpg",
+		title: "august 25 event, mystical sunset",
+		image: august_25,
 	},
 ];
 
