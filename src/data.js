2aa4f4ac826b634d879c7aa363eaeeaca878bdca
Leotori <leovid2392@gmail.com>
2021-08-26 10:43:11 -0500

    removing august 28 event

--- src/data.js
+++ src/data.js
@@ -5,9 +5,9 @@ import august_28 from "./assets/slider/august_28.jpeg";
 const sliderData = [
 	{
 		id: 1,
-		url: "https://www.eventbrite.com.mx/e/mystical-sunset-tickets-167620566627",
-		title: "august 25 event, mystical sunset",
-		image: august_25,
+		url: "https://www.ticketfairy.com/event/francesca-lombardo-28aug2021/?pk=cd0c2ff489c65a3eb7f1836cfaefb35f5a68b0d75c1ddb8b72c3add29d138f8f",
+		title: "august 28, especial dinner with francesca lombardo",
+		image: august_28,
 	},
 	{
 		id: 2,
