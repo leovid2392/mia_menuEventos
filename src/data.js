1babebc89746c6068f052b4895bebd6edfeaef67
Leotori <leovid2392@gmail.com>
2021-08-29 10:24:08 -0500

    removing august 28 event, and setting default img

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 import React from "react";
 import august_25 from "./assets/slider/august_25.jpeg";
-import august_28 from "./assets/slider/august_28.jpeg";
+import default_img from "./assets/slider/default.jpeg";
 
 const sliderData = [
 	{
 		id: 1,
-		url: "https://www.ticketfairy.com/event/francesca-lombardo-28aug2021/?pk=cd0c2ff489c65a3eb7f1836cfaefb35f5a68b0d75c1ddb8b72c3add29d138f8f",
-		title: "august 28, especial dinner with francesca lombardo",
-		image: august_28,
+		url: "#",
+		title: "cocktail",
+		image: default_img,
 	},
 	{
 		id: 2,
-		url: "https://www.ticketfairy.com/event/francesca-lombardo-28aug2021/?pk=cd0c2ff489c65a3eb7f1836cfaefb35f5a68b0d75c1ddb8b72c3add29d138f8f",
-		title: "august 28, especial dinner with francesca lombardo",
-		image: august_28,
+		url: "#",
+		title: "cocktail",
+		image: default_img,
 	},
 ];
 
