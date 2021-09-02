a9ca06944cdd85381c04ee3aaf29c44e26602ab3
Leotori <leovid2392@gmail.com>
2021-09-01 20:50:31 -0500

    september 02 event added

--- src/data.js
+++ src/data.js
@@ -1,13 +1,14 @@
 import React from "react";
 import september_05 from "./assets/slider/september_05.jpeg";
+import september_02 from "./assets/slider/september_02.jpeg";
 // import default_img from "./assets/slider/default.jpeg";
 
 const sliderData = [
 	{
 		id: 1,
-		url: "https://www.ticketfairy.com/event/david-penn-5sep2021/",
-		title: "special dinner with david peen",
-		image: september_05,
+		url: "https://www.eventbrite.com/e/entradas-mix-mag-session-tulum-169019595159",
+		title: "mixmag mexico, roderic mehill",
+		image: september_02,
 	},
 	{
 		id: 2,
