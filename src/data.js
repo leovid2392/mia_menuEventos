35f3a2657472e21605ca35a32f5be5061c1bb6c5
Leotori <leovid2392@gmail.com>
2021-09-03 03:36:30 -0500

    september 02 event removed

--- src/data.js
+++ src/data.js
@@ -8,12 +8,7 @@ import september_17 from "./assets/slider/september_17.jpeg";
 // import default_img from "./assets/slider/default.jpeg";
 
 const sliderData = [
-	{
-		id: 1,
-		url: "https://www.eventbrite.com/e/entradas-mix-mag-session-tulum-169019595159",
-		title: "mixmag mexico, roderic mehill",
-		image: september_02,
-	},
+	
 	{
 		id: 2,
 		url: "https://www.ticketfairy.com/event/david-penn-5sep2021/",
